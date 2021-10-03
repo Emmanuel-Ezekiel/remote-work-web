b7e4c6391d302a3e0f7a47beb59f0b3d22d65795
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-10-03 13:06:42 +0100

    change feature

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,30 +4,35 @@ import { FaBars, FaSearch } from 'react-icons/fa'
 function Navbar() {
     return (
         <>
-          <nav className='nav'>
+          <header className='nav'>
               <div className="nav-center">
                   <div className="nav-header">
                       <img alt="we work remotely" className="nav-logo" src="https://weworkremotely.com/assets/IsotypeV2-1ebe3dd57673f3e8d02b7490bc0faaef55d6a95d3a4aaf17298bd3ed503ae7fe.svg"></img>
                       <button className="btn toggle-btn"> <FaBars/></button>
                   </div>
-                  <ul className="nav-links">
-                      <li>
-                          <button className="link-btn">categories</button>
+
+                  <nav className="nav-bar">
+                    <ul className="nav-links">
+                      <li className="link-btn">
+                          <a href="#">Categories</a>
                       </li>
-                      <li>
-                          <button className="link-btn">communities</button>
+                      <li className="link-btn">
+                          <a href="#">Community</a>
                       </li>
-                      <li>
-                          <button className="link-btn">job seekers</button>
+                      <li className="link-btn">
+                          <a href="#">Job Seekers</a>
                       </li>
-                      <li>
-                          <button className="link-btn">employers</button>
-                      </li>
-                    <button className="btn search-btn"> <FaSearch/></button>
+                      <li className="link-btn">
+                          <a href="#">Employers</a>
+                      </li>  
+                    <li className="link-btn search-btn" > <FaSearch/></li>
+                    <li className="link-btn">
+                        <a id="post-btn" href="#" >post a job</a>
+                    </li>
                   </ul>
-                  
+                </nav>
               </div>
-            </nav>  
+            </header>  
         </>
     )
 }
