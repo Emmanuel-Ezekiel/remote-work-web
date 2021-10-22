8e10a8ea8d94ab00600ac242dade39d8ffac5e0e
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-10-22 11:17:10 +0100

    add new commit

--- src/components/JobDescription.js
+++ src/components/JobDescription.js
@@ -4,30 +4,31 @@ import Data from './Data'
 import { BsFillArrowLeftSquareFill } from 'react-icons/bs';
 
 const JobDescription = () => {
-     const [description, setDescription] = useState('job description')
-     const [title, setTitle] = useState()
-     const [location, setLocation] = useState()
-     const [image, setImage] = useState()
-    const { id } = useParams();
+     const [description, setDescription] = useState("")
+     const [title, setTitle] = useState("")
+     const [location, setLocation] = useState("")
+     const { id } = useParams();
 
     useEffect(() => {
         const newDescription = Data.find((job) => job.id === parseInt(id));
         setTitle(newDescription.jobTitle);
         setDescription(newDescription.companyName);
         setLocation(newDescription.Location);
-
     }, [])
     return (
-        <div>
-            
-      <Link to='/' className="link-btn">
-         <span> <BsFillArrowLeftSquareFill/></span><span className='description-btn'> Back Home</span>
+    <section>
+     <div className="description-btn">
+        <Link to='/' className="link-btn">
+         <span><BsFillArrowLeftSquareFill/></span><span className="home-btn"> Back Home</span>
        </Link>
-
-       <h2>{description}</h2>
+       <article className="description">
+        <h2>{description}</h2>
        <h2>{title}</h2>
-       <h2>{location}</h2>
-        </div>
+       <button className="location-btn">{location}</button>
+       </article>
+       
+     </div>
+    </section>
     )
 }
 
